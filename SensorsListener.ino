270a4b38efa3543ea9f457c7f1ad49c59222fa7c
dkoldaev <dkoldyaev@gmai.com>
2024-08-25 23:38:39 +0400

    phrases are moved to side file

--- SensorsListener.ino
+++ SensorsListener.ino
@@ -1,8 +1,11 @@
 #include "LightSensor.h"
 #include "TemperatureSensor.h"
 #include "SensorNode.h"
+#include "Phrases.h"
+#define BUFFER_SIZE 50
 
-bool debugMode = true;  // Флаг отладки
+bool debugMode = false;  // Флаг отладки
+int outerValue = 50;
 
 SensorNode* head = nullptr;  // Указатель на голову списка
 
@@ -24,19 +27,35 @@ void setup() {
 
   // Добавляем датчики в связный список
   addSensor(new LightSensor(A5, 0, 255));
-  addSensor(new TemperatureSensor(A7, -40, 125));
+  addSensor(new TemperatureSensor(A7, 0, 2));
 }
 
 void loop() {
   SensorNode* current = head;
+  int sensorIndex = 0;
+  char buffer[BUFFER_SIZE];
+
   while (current != nullptr) {
     Sensor* sensor = current->sensor;
-    Serial.print(sensor->getName());
-    Serial.print(": ");
-    Serial.println(debugMode ? sensor->rawValue() : sensor->readValue(0, 255));
+    if (debugMode) {
+      Serial.print(sensor->getName());
+      Serial.print(" Out: ");
+      Serial.print(sensor->readValue(0, outerValue));
+      Serial.print(" Raw: ");
+      Serial.print(sensor->rawValue());
+      Serial.println();
+    }
+
+    // Извлечение и вывод фразы из PROGMEM
+    strcpy_P(buffer, (char*)pgm_read_word(&(phrases[sensorIndex][sensor->readValue(0, outerValue) % 4])));
+    Serial.print(buffer);
+    Serial.print(" ");
+    
+    Serial.println();
     current = current->next;
+    sensorIndex++;
   }
   Serial.println(); // Пустая строка для разделения данных
 
-  delay(1000); // Задержка 1 секунда перед следующим чтением
+  delay(5000); // Задержка 1 секунда перед следующим чтением
 }
